d40ac548ed369671b59bf7920c03523741a96161
KaterinaValiuk <e5054404@gmail.com>
2021-08-21 21:25:08 +0300

    docs: string decoded

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 8 */
 const MORSE_TABLE = {
     '.-':     'a',
     '-...':   'b',
@@ -35,12 +36,45 @@ const MORSE_TABLE = {
     '---..':  '8',
     '----.':  '9',
     '-----':  '0',
+    '': ' ',
+};
+
+const sumbul = {
+    '10': '.',
+    '11': '-',
+    '**': '',
+    '00': '',
 };
 
 function decode(expr) {
-    // write your solution here
+   //let expr = "00101010100000000010001011101000101110100000111111**********00001011110000111111000010111000101110100000111010";
+    let arr = [];
+    let symbolNum = expr.length / 10;
+       
+   for (let i = 0; i < symbolNum; i++) {  
+       let sumbulArr = expr.slice(i * 10, i * 10 + 10);
+       let sumbulMorse = `${sumbul[sumbulArr.slice(0, 2)]}${sumbul[sumbulArr.slice(2, 4)]}${sumbul[sumbulArr.slice(4, 6)]}${sumbul[sumbulArr.slice(6, 8)]}${sumbul[sumbulArr.slice(8)]}`;   
+       arr[i] = sumbulMorse;
+    }
+    
+    console.log(arr);
+    
+
+    arr.forEach(function(item, i, arr) {
+        arr[i] = MORSE_TABLE[arr[i]];
+        
+
+    });
+    console.log(arr);
+    let decode = arr.join('');
+    return decode;
+   
+
+
+    
+
 }
 
 module.exports = {
     decode
-}
+};
